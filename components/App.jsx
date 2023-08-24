208da8c962156dd014dd28ddbe1fa8061b68a46c
Jack Singh <jacksingh28092002@gmail.com>
2023-08-24 22:28:11 +0530

    refactored package.json start script

---
+++ components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Header from "./Header.jsx";
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CreateIcon from '@material-ui/icons/Create';
+function Header() {
+  return (
+    <header>
+      <h1><CreateIcon/>  I'll Keep It</h1>
+    </header>
+  );
+}
+export default Header;
